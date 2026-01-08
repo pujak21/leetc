f8c87a244d5c96639e0f051e919bcf90f80fb2e2
pujak21 <pujaojha7187@gmail.com>
2026-01-08 18:28:06 +0530

    Time: 0 ms (100%), Space: 43.8 MB (43.22%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,10 +1,48 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        //Set<Integer> set = new TreeSet<>();
-        //ArrayList<Integer> list = new ArrayList<>();
+        int i = m-1;
+        int j = m+n-1;
+        int k = n-1;
 
-        int[] temp = new int[m+n];
+        while(i>=0 && k>=0){
+            if(nums1[i] > nums2[k]){
+                nums1[j] = nums1[i];
+                i--;
+            }
+            else{
+                nums1[j] = nums2[k];
+                k--;
+            }
+            j--;
+        }
+
+        // if nums2 still has elements but 1st array elements have been traversed
+
+        while(k>=0){
+            nums1[j] = nums2[k];
+            k--;
+            j--;
+        }
+
+
+
+
+
+
+
+        //another brute froce
+
+        /*int k = 0;
+        for(int i = m; i<= m+n-1; i++){
+            nums1[i] = nums2[k];
+            k++;
+        }
+        Arrays.sort(nums1);*/
+
+        //brute force method
+
+        /*int[] temp = new int[m+n];
 
         int k = 0;
         for(int i = 0; i<m; i++){
@@ -18,8 +56,8 @@ class Solution {
         Arrays.sort(temp);
         for(int i = 0; i< m+n; i++){
             nums1[i] = temp[i];
-        }
+        }*/
         
-        //return list;
+        
     }
 }
